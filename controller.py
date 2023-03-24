ed0c6fb1d941a8a1bacc4b03f3cfa86e69e63888
ABHISHEK PANDEY <abhishekpandeyite@gmail.com>
2023-03-25 02:28:05 +0530

    Updated Method's Docstring and ReadMe File

--- controller.py
+++ controller.py
@@ -1,3 +1,25 @@
+"""
+This is a Flask application that interacts with Firestore to provide API endpoints for managing user preferences and favorites.
+
+The following routes are available:
+
+GET '/preferences/<user_id>':
+    Retrieve user preferences based on the user ID.
+
+POST '/preferences/<user_id>':
+    Update user preferences based on the user ID.
+
+GET '/preferences/<user_id>/favorites':
+    Retrieve user favorites based on the user ID.
+
+POST '/preferences/<user_id>/favorites':
+    Add a user favorite recipe based on the user ID.
+
+DELETE '/preferences/<user_id>/favorites/<recipe_id>':
+    Delete a user favorite recipe based on the user ID and recipe ID.
+
+"""
+
 from flask import Flask, jsonify, request
 import firebase_admin
 from firebase_admin import credentials, firestore
@@ -10,11 +32,19 @@ cred = credentials.Certificate('firebase_credentials.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-# API endpoint to get user preferences
-
 
 @app.route('/preferences/<user_id>', methods=['GET'])
 def get_user_preferences(user_id):
+    """
+    Retrieve user preferences based on the user ID.
+
+    Args:
+    user_id (str): A string representing the unique ID of the user.
+
+    Returns:
+    A JSON object representing the user preferences if found, or an error message if the user is not found.
+    """
+
     doc_ref = db.collection('user_preference').document(user_id)
     doc = doc_ref.get()
     if doc.exists:
@@ -22,11 +52,19 @@ def get_user_preferences(user_id):
     else:
         return jsonify({'error': 'User not found'})
 
-# API endpoint to update user preferences
-
 
 @app.route('/preferences/<user_id>', methods=['POST'])
 def update_user_preferences(user_id):
+    """
+    Update user preferences based on the user ID.
+
+    Args:
+    user_id (str): A string representing the unique ID of the user.
+
+    Returns:
+    A JSON object representing the success message if the preferences are updated successfully.
+    """
+
     doc_ref = db.collection('user_preference').document(user_id)
     doc_data = request.get_json()
     doc_ref.set(doc_data)
@@ -35,45 +73,69 @@ def update_user_preferences(user_id):
 
 @app.route('/preferences/<user_id>/favorites', methods=['GET'])
 def get_user_favorites(user_id):
-    # Get the user favorites data from Firestore
+    """
+    Retrieve user favorites based on the user ID.
+
+    Args:
+    user_id (str): A string representing the unique ID of the user.
+
+    Returns:
+    A JSON object representing the user favorites if found.
+    """
+
     doc_ref = db.collection("user_preference").document(user_id)
     favorites_ref = doc_ref.collection("favorites_recipe")
     favorites = []
     for fav in favorites_ref.stream():
         favorites.append(fav.to_dict())
 
-    # Return the user favorites data as a JSON response
     response = {'favorites': favorites}
     return jsonify(response), 200
 
 
 @app.route('/preferences/<user_id>/favorites', methods=['POST'])
 def add_user_favorite(user_id):
-    # Get the request body JSON data
+    """
+    Add a user favorite recipe based on the user ID.
+
+    Args:
+    user_id (str): A string representing the unique ID of the user.
+
+    Returns:
+    A JSON object representing the success message and the generated favorite recipe ID.
+    """
+
     request_data = request.get_json()
 
-    # Add the user favorite data to Firestore
     doc_ref = db.collection("user_preference").document(user_id)
     favorites_ref = doc_ref.collection("favorites_recipe")
     favorite_doc_ref = favorites_ref.document()
     favorite_doc_ref.set(request_data)
     favorite_id = favorite_doc_ref.id
 
-    # Return success message with the generated favorite recipe ID
     response = {'message': 'User favorite recipe added successfully.',
                 'favorite_id': favorite_id}
     return jsonify(response), 200
 
 
 @app.route('/preferences/<user_id>/favorites/<recipe_id>', methods=['DELETE'])
-def delete_user_favorite(user_id, recipe_id):
-    # Delete the user favorite data from Firestore
+def delete_user_favorite_recipe(user_id, recipe_id):
+    """
+    API endpoint to delete user favorite.
+
+    Args:
+        user_id (str): The user ID for whom a favorite recipe is to be deleted.
+        recipe_id (str): The favorite recipe ID to be deleted.
+
+    Returns:
+        A JSON response confirming the favorite recipe was deleted successfully.
+    """
+
     doc_ref = db.collection("user_preference").document(user_id)
     favorites_ref = doc_ref.collection("favorites_recipe")
     favorites_ref.document(recipe_id).delete()
 
-    # Return success message
-    response = {'message': 'User favorite deleted successfully.'}
+    response = {'message': 'User favorite recipe deleted successfully.'}
     return jsonify(response), 200
 
 
